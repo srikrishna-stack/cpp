710ea27ce0ebed40209a80db25b841526244a9b2
sri krishna <82864734+srikrishna-stack@users.noreply.github.com>
2021-06-11 11:45:19 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if (n==0){
+        cout << "If excutes " << endl;
+
+    }
+    else{
+        cout << "I dont think If is right :p"<<endl;
+        
+    }
+    cout << "EXecutes anyway" << endl;
+    return 0;
+
+}
